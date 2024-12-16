6a2ab1d6e5466c4b773452ace78dc7756dd650db
Sébastien Bédard <sebedard13@gmail.com>
2024-12-16 18:06:58 -0500

    start chap02 draw with gpu

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Rust Animation programming
 
-This is a small project to gpu rendering and animation programming in Rust. This project follows the book C++ Game
-Animation
-Programming by Michael Dunsky and Gabor Szauer from 2023. While the book is in C++, I will try to implement the examples
-in Rust, but the main goal is to learn about animation programming and not to make a direct translation from C++ to
-Rust. In the book, the authors use the OpenGL or Vulkan API to render the animations, but I will use the wgpu-rs crate.
+This is a small project to gpu rendering and animation programming in Rust. This project follows a little bit the book
+C++ Game Animation Programming by Michael Dunsky and Gabor Szauer from 2023 and
+this [Learn Wgpu](https://sotrh.github.io/learn-wgpu/) guide. While the book is in C++, I will try to implement the
+examples in Rust, but the main goal is to learn about animation programming and not to make a direct translation from
+C++ to Rust. In the book, the authors use the OpenGL or Vulkan API to render the animations, but I will use the wgpu-rs
+crate.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod state;
+
+use crate::state::State;
 use anyhow::Context;
 use anyhow::Result;
 use log::{info, log, warn};
@@ -8,55 +11,59 @@ use winit::{
     window::WindowBuilder,
 };
 
-pub fn run() -> Result<()> {
-    env_logger::builder().filter_level(log::LevelFilter::Info).init();
+pub async fn run() -> Result<()> {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
     let event_loop = EventLoop::new().context("Error creating the event loop")?;
-    let window = WindowBuilder::new().build(&event_loop).context("Error creating the window")?;
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .context("Error creating the window")?;
 
-    event_loop.run(move |event, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                event:
-                KeyEvent {
-                    state: ElementState::Pressed,
-                    physical_key: PhysicalKey::Code(KeyCode::Escape),
-                    ..
-                },
-                ..
-            } => control_flow.exit(),
-            WindowEvent::KeyboardInput {
-                event: key_event,
-                ..
-            } => {
-                handle_key_event(&key_event);
+    let mut state = State::new(&window).await;
+
+    event_loop
+        .run(|event, control_flow| match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => {
+                if !state.input(event) {
+                    match event {
+                        WindowEvent::CloseRequested
+                        | WindowEvent::KeyboardInput {
+                            event:
+                                KeyEvent {
+                                    state: ElementState::Pressed,
+                                    physical_key: PhysicalKey::Code(KeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        } => control_flow.exit(),
+                        WindowEvent::KeyboardInput {
+                            event: key_event, ..
+                        } => {
+                            handle_key_event(&key_event);
+                        }
+                        WindowEvent::Resized(physical_size) => {
+                            state.resize(*physical_size);
+                        }
+                        _ => {}
+                    }
+                };
             }
-            _ => {}
-        },
-        Event::DeviceEvent {
-            ref event,
-            ..
-        } => {
-            match event {
-                DeviceEvent::MouseMotion {
-                    delta,
-                } => {
+            Event::DeviceEvent { ref event, .. } => match event {
+                DeviceEvent::MouseMotion { delta } => {
                     info!("Mouse motion: {:?}", delta);
                 }
-                DeviceEvent::Button {
-                    button,
-                    state,
-                } => {
+                DeviceEvent::Button { button, state } => {
                     info!("Mouse button: {:?} {:?}", button, state);
                 }
                 _ => {}
-            }
-        }
-        _ => {}
-    }).context("Error in event loop")?;
+            },
+            _ => {}
+        })
+        .context("Error in event loop")?;
     Ok(())
 }
 
@@ -65,5 +72,5 @@ fn handle_key_event(event: &KeyEvent) {
 }
 
 fn main() -> Result<()> {
-    run()
+    pollster::block_on(run())
 }
